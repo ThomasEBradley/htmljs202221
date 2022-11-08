562baa05f1948ff86f946cc04986c1058681ca7f
ThomasEBradley <tebradley@email.neit.edu>
2022-11-08 11:14:52 -0500

    End of Week 6 Day 1 Class

--- Assignment5/script.js
+++ Assignment5/script.js
@@ -4,19 +4,20 @@ var ctx = canvas.getContext("2d")
 
 ctx.fillStyle = "yellow"
 ctx.strokeStyle = "black"
-ctx.lineWidth = "5px"
+ctx.lineWidth = "5"
 ctx.fillRect(85,302,100,100)
 ctx.strokeRect(85,302,100,100)
 
 ctx.beginPath()
 ctx.strokeStyle = "rgb(255,0,0)"
+ctx.lineWidth = "5"
 ctx.moveTo(85,682);
 ctx.lineTo(278,549);
 ctx.stroke();
 
 ctx.fillStyle = "#ffff00"
 ctx.strokeStyle = "red"
-ctx.lineWidth = "5px"
+ctx.lineWidth = "5"
 ctx.beginPath()
 ctx.arc(385,442,66,0,7)
 ctx.closePath()
@@ -26,6 +27,7 @@ ctx.stroke()
 ctx.beginPath()
 ctx.fillStyle = "#ff00ff"
 ctx.strokeStyle = "#00ffff"
+ctx.lineWidth = "5"
 ctx.moveTo(558,309)
 ctx.lineTo(667,284)
 ctx.lineTo(724,380)
@@ -38,6 +40,7 @@ ctx.fill()
 ctx.beginPath()
 ctx.fillStyle = "#ffff00"
 ctx.strokeStyle = "rgb(32,32,32)"
+ctx.lineWidth = "5"
 ctx.moveTo(636,497)
 ctx.lineTo(668,554)
 ctx.lineTo(733,567)
