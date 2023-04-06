2b4c9df175ea14568f2fa92fde4f0540a3ad100a
ThomasEBradley <tebradley@email.neit.edu>
2023-04-05 20:17:35 -0400

    Level 1.1

---
+++ gds137/level1/level1.1/level1.1.js
@@ -0,0 +1,11 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d")
+
+function Ball()
+{
+    x = 0
+    y = 0
+    width = 300
+    height = 300
+    fillStyle = "black"
+}
